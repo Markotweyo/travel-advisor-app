87ebc7fc22b5a3880dae0f2ac7b0a2cd8f6a426c
Mark Otweyo <markotweyo99@gmail.com>
2021-10-15 01:12:52 +0300

    Working with APIs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {CssBaseline, Grid} from '@material-ui/core'
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
+import {getPlaceData} from './api';
 
 
 function App() {
+
+  const [places, setPlaces] = useState([])
+
+  const [coordinates, setCoordinates] = useState({})
+  const [bounds, setBounds] = useState(null)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}})=>{
+      setCoordinates({lat: latitude, lng: longitude})
+
+    })
+    
+  }, [])
+
+  
+  useEffect(() => {
+    
+    getPlaceData(bounds.sw, bounds.ne)
+    .then((data)=>{
+      console.log(data)
+        setPlaces(data)
+    })
+    
+  }, [coordinates, bounds])
+
   return (
     <div>
       <CssBaseline/>
@@ -17,7 +43,11 @@ function App() {
         </Grid>
 
         <Grid item xs ={12} md={8}>
-          <Map/>
+          <Map
+          setCoordinates={setCoordinates}
+          setBounds={setBounds}
+          coordinates={coordinates}
+          />
         </Grid>
 
       </Grid>
---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const URL ='https://travel-advisor.p.rapidapi.com/hotels/list-in-boundary'
+
+
+
+export const getPlaceData = async (sw, ne)=> {
+    try {
+        const {data: {data }}= await axios.get(URL, { 
+            params: {
+            bl_latitude: sw.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+            tr_latitude: ne.lat,
+            
+          },
+          headers: {
+            'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            'x-rapidapi-key': '70d787b3e4msh1e0f4206955c691p1225f0jsn1df953ddc958'
+          }
+        })
+        
+        return data
+
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
