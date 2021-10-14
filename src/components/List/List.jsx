b8333511fefbdac227258d3f9b921b71b1392af1
Mark Otweyo <markotweyo99@gmail.com>
2021-10-14 18:33:14 +0300

    Setting up react app and adding Header component

---
+++ src/components/List/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import useStyles from './styles'
+
+const List = () => {
+    return (
+        <div>
+            List
+        </div>
+    )
+}
+
+export default List
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import useStyles from './styles'
+
+const Map = () => {
+    return (
+        <div>
+            Map
+        </div>
+    )
+}
+
+export default Map
