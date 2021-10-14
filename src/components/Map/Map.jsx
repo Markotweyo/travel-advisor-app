f38c56be919ffd2c177116e29cc66c6e551847a1
Mark Otweyo <markotweyo99@gmail.com>
2021-10-14 19:33:18 +0300

    Adding the Map Component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import useStyles from './styles'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+import useStyles from './styles';
 
 const Map = () => {
+    const classes= useStyles();
+    const isMobile=useMediaQuery('(min-width: 600 px)');
+
+    const coordinates={lat: 0, lng: 0};
     return (
-        <div>
-            Map
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key:'AIzaSyDvCuWRuecqsTsCFMTQq-7uuRbAl4HGInk'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+
         </div>
     )
 }
